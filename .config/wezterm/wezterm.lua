fd2611b67ef4e59f10a103bdbb1e162f304136f6
Florian Breit <breit.florian@icloud.com>
2025-03-09 19:30:24 +0100

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require('wezterm')
+
+return {
+  -- fonts
+  font = wezterm.font 'Hack Nerd Font Mono',
+  font_size = 13,
+  -- tab bar style
+  hide_tab_bar_if_only_one_tab = true,
+  -- colorscheme
+  color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+}
