c42aa4b8cbdeccf1da0209cb7d2f0141e351bc0f
Florian Breit <breit.florian@icloud.com>
2025-03-09 19:44:14 +0100

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,14 @@ return {
   font = wezterm.font 'Hack Nerd Font Mono',
   font_size = 13,
   -- tab bar style
+  enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = true,
+  -- other appearance settings
+  macos_window_background_blur = 30,
+  -- window size
+  initial_cols = 136,
+  initial_rows = 42,
   -- colorscheme
   color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
 }
