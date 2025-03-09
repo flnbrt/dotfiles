625c1fa1e88001ee0093e6ce51b69829326a8d51
Florian Breit <breit.florian@icloud.com>
2025-03-09 12:53:33 +0100

    change PS profile installer

--- installer/install_windows_dotfiles.ps1
+++ installer/install_windows_dotfiles.ps1
@@ -1,16 +1,20 @@
+Write-Host ""
 Write-Host "#########################################"
 Write-Host "# Windows PowerShell Dotfiles Installer #"
 Write-Host "#########################################"
 Write-Host ""
-Write-Host "-----------------------------------------"
-Write-Host "Installing Windows PowerShell Dotfiles..."
+Write-Host "----------------------------------------"
+Write-Host "Installing Windows PowerShell Profile..."
+
+$profileDir = Split-Path -Parent $PROFILE
+if (!(Test-Path $profileDir)) {
+    New-Item -ItemType Directory -Path $profileDir -Force | Out-Null
+}
 
 try {
     $url = "https://raw.githubusercontent.com/flnbrt/dotfiles/main/windows/Microsoft.PowerShell_profile.ps1"
-    Write-Host "Installing PowerShell Profile..."
     Invoke-WebRequest -Uri $url -OutFile $PROFILE -UseBasicParsing
     Write-Host "Profile Installation Successfully!"
-    Write-Host "PowerShell Profile is up to date."
 } catch {
     Write-Host "Failed to install PowerShell Profile: $_"
 }
