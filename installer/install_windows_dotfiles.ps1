a6ff85bc15f702385e23b70b75e837377b0db5d3
Florian Breit <breit.florian@icloud.com>
2025-03-09 13:29:13 +0100

    add nvim config installer for windows

--- installer/install_windows_dotfiles.ps1
+++ installer/install_windows_dotfiles.ps1
@@ -3,6 +3,26 @@ Write-Host "#########################################"
 Write-Host "# Windows PowerShell Dotfiles Installer #"
 Write-Host "#########################################"
 Write-Host ""
+
+#######################
+# Dotfiles Repository #
+#######################
+
+Write-Host "----------------------------------"
+Write-Host "Installing Dotfiles Repository..."
+
+$dotfilesDir = [Environment]::GetFolderPath("MyDocuments") + "\Github\dotfiles"
+if (!(Test-Path $dotfilesDir)) {
+    git clone https://github.com/flnbrt/dotfiles.git $dotfilesDir
+} else {
+    git -C $dotfilesDir pull
+}
+
+######################
+# PowerShell Profile #
+######################
+
+Write-Host ""
 Write-Host "----------------------------------------"
 Write-Host "Installing Windows PowerShell Profile..."
 
@@ -18,3 +38,31 @@ try {
 } catch {
     Write-Host "Failed to install PowerShell Profile: $_"
 }
+
+########################
+# Neovim Configuration #
+########################
+
+Write-Host ""
+Write-Host "----------------------------------"
+Write-Host "Installing Neovim Configuration..."
+
+# install NeoVim with WinGet, if not already present on system
+if (!$(Get-Command nvim -ErrorAction SilentlyContinue)) {
+    Write-Host "Neovim Not found, installing with WinGet..."
+    winget install Neovim.Neovim
+}
+
+# create nvim directory if not already present
+$nvimDir = "$env:LOCALAPPDATA\nvim"
+if (!(Test-Path $nvimDir)) {
+    Write-Host "Creating nvim directory..."
+    New-Item -ItemType Directory -Path $nvimDir -Force | Out-Null
+}
+
+# Link nvim configuration files
+$localNvimConfig = Join-Path $env:LOCALAPPDATA "nvim"
+$dotfilesNvimConfig = Join-Path $dotfilesDir ".config" "nvim"
+
+Write-Host "Creating Symbolic Link to nvim configuration..."
+New-Item -Path $localNvimConfig -ItemType SymbolicLink -Value $dotfilesNvimConfig -Force
