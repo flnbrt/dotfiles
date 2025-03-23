b905eeba2b488911d5e589d8126bbca2ab414975
Florian Breit <breit.florian@icloud.com>
2025-03-23 22:23:29 +0100

    change wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,18 +1,35 @@
 local wezterm = require('wezterm')
+local config = {}
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
-return {
-  -- fonts
-  font = wezterm.font 'Hack Nerd Font Mono',
-  font_size = 13,
-  -- tab bar style
-  enable_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = true,
-  -- other appearance settings
-  macos_window_background_blur = 30,
-  -- window size
-  initial_cols = 136,
-  initial_rows = 42,
-  -- colorscheme
-  color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-}
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- fonts
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font_size = 14
+
+-- tab bar style
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+-- other appearance settings
+config.macos_window_background_blur = 30
+
+-- window size
+config.initial_cols = 136
+config.initial_rows = 42
+
+-- colorscheme
+config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
+
+-- plugins
+-- tabline
+tabline.setup()
+
+-- return config to wezterm
+return config
